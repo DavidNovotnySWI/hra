ef10d7871d4e17434633027c61c69c0f479918d0
DavidNovotnySWI <d_novotny@utb.cz>
2025-05-31 17:43:45 +0200

    Update 3
    Update 3: level and weapon system

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,31 +1,59 @@
 extends CharacterBody2D
  
 var speed : float = 150
-var health: float = 100:
+var health : float = 100.0 :
 	set(value):
 		health = value
 		%Health.value = value
-		
+ 
 var nearest_enemy : CharacterBody2D
 var nearest_enemy_distance : float = INF
+ 
+var level : int = 1:
+	set(value):
+		level = value
+		%Level.text = str(value)
+		%Options.show_option()
+		if level >= 2:
+			%XP.max_value = 20
+		elif level >= 7:
+			%XP.max_value = 40
+var XP : int = 0:
+	set(value):
+		XP = value
+		%XP.value = value
+var total_XP : int = 0
  
 func _physics_process(delta):
 	if is_instance_valid(nearest_enemy):
 		nearest_enemy_distance = nearest_enemy.separation
 	else:
 		nearest_enemy_distance = INF
-	
 	velocity = Input.get_vector("left","right","up","down") * speed
 	move_and_collide(velocity * delta)
-
+	check_XP()
+ 
 func take_damage(amount):
 	health -= amount
 	#print(amount)
+ 
+func check_XP():
+	if XP > %XP.max_value:
+		XP -= %XP.max_value
+		level += 1
+ 
+ 
+func gain_XP(amount):
+	XP += amount
+	total_XP += amount
 
-func _on_self_damage_body_entered(body: Node2D) -> void:
+func _on_timer_timeout():
+	%Collision.set_deferred("disabled", true)
+	%Collision.set_deferred("disabled", false)
+	
+func _on_self_damage_body_entered(body):
 	take_damage(body.damage)
 
-
-func _on_timer_timeout() -> void:
-	%Collision.set_deferred("disabled",true)
-	%Collision.set_deferred("disabled",false)
+func _on_magnet_area_entered(area):
+	if area.has_method("follow"):
+		area.follow(self)
